11992ac94f50cc8faca1c1b9ea59d802e4f2a072
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-04-15 15:13:03 +0900

    feat(create):Create画面追加

--- src/app/create/create/create.component.ts
+++ src/app/create/create/create.component.ts
@@ -1,29 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
 
   form = this.fb.group({
-    name: ['', [
-      Validators.required,
-      Validators.maxLength(40)
-    ]],
-    gender: ['', [
-      Validators.required,
-      Validators.pattern(/male | female/)
-    ]]
+    name: ['', [Validators.required, Validators.maxLength(40)]],
+    gender: ['', [Validators.required, Validators.pattern(/male|female/)]],
   });
 
-  constructor(
-    private fb: FormBuilder
-  ) { }
-
-  ngOnInit(): void {
+  get nameControl() {
+    return this.form.get('name') as FormControl;
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
