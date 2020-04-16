12741e897b9ea18d8d13f8287202799cdb679638
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-04-16 14:12:27 +0900

    feat(home): ホーム画面追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
     path: 'welcome',
-    loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule)
+    loadChildren: () =>
+      import('./welcome/welcome.module').then((m) => m.WelcomeModule),
   },
   {
     path: 'create',
-    loadChildren: () => import('./create/create.module').then(m => m.CreateModule)
+    loadChildren: () =>
+      import('./create/create.module').then((m) => m.CreateModule),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
