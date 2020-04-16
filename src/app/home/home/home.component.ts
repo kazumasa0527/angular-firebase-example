499218c5d478d909244ca47469de19947775137a
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-04-17 08:26:01 +0900

    feat(home): Petコンポーネント追加

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Pet } from 'src/app/interfaces/pet';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  pet: Pet = {
+    name: 'テストペット',
+    exp: 200,
+    level: 4,
+    avatarURL: '/assets/images/pet-1.png',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
