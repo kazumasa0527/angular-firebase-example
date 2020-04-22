31089772d211dbce9a5cd728e4af239df4161095
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-04-22 12:09:44 +0900

    style(git-hook): レベル計算ロジック修正

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -5,41 +5,34 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-const expTable = [
-  20,
-  40,
-  100,
-  250,
-  500,
-  1000,
-  1500,
-  4000,
-  10000
-];
+const expTable = [20, 40, 100, 250, 500, 1000, 1500, 4000, 10000];
 
 const EARN_EXPERIENCE = 10;
 
 export const gitHook = functions.https.onRequest(async (request, response) => {
-  const pets = await db.collection('pets')
+  const pets = await db
+    .collection('pets')
     .where('ownerGitHubId', '==', request.body.sender.id)
-    .get()
+    .get();
 
-    const pet = pets.docs[0].data();
+  const pet = pets.docs[0].data();
 
-    let level = 1;
-    expTable.some(nextExp => {
-      if(pet.exp + EARN_EXPERIENCE>= nextExp) {
-        level++;
-        return false;
-      } else {
-        return true;
-      }
-    });
+  let level = 1;
+  expTable.some((nextExp) => {
+    if (pet.exp + EARN_EXPERIENCE >= nextExp) {
+      level++;
+      return false;
+    } else {
+      return true;
+    }
+  });
 
-    const increment = admin.firestore.FieldValue.increment(EARN_EXPERIENCE);
-    pets.docs.forEach(doc => doc.ref.update({
+  const increment = admin.firestore.FieldValue.increment(EARN_EXPERIENCE);
+  pets.docs.forEach((doc) =>
+    doc.ref.update({
       exp: increment,
-      level
-    }));
+      level,
+    })
+  );
   response.send('success');
 });
